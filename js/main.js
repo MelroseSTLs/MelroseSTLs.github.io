5fea96530af46430df4985f8ab7ced62d5917b5d
Conlan <conlanc@gmail.com>
2016-10-12 14:08:21 -0400

    Handle 'site' param if it exists

--- js/main.js
+++ js/main.js
@@ -17,3 +17,21 @@ function setMainFrame(setFrameTo, isStudentPage, elementToSetActive) {
   if (!!setActive) { setActive.className += " active"; }
   if (!!oldActiveClass) { oldActiveClass.className = oldActiveClass.className.replace('active',''); }
 }
+
+function getURLParam(name) {
+  // http://stackoverflow.com/a/11582513/1709894
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+}
+
+function isURL(str) {
+  var regex = new RegExp('^(http|https)://'); // Test if is HTTP or HTTPS url
+  if (regex.test(str)) { return true; }
+  return false;
+}
+
+function handleParams() {
+  var siteName = getURLParam('site');
+  if (isURL(siteName)) {
+    setMainFrame(siteName, false, null);
+  }
+}
